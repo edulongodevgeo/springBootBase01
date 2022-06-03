a0eef11554d26de4aad9dd50ca52cb1be1e7d7db
Eduardo Longo <65884093+edulongodevgeo@users.noreply.github.com>
2022-06-03 18:56:12 -0300

    Add files via upload

---
+++ springBootBase01/ProjetoBackEnd/primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/PrimeiroExemploApplication.java
@@ -0,0 +1,15 @@
+package com.teste.primeiroexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroExemploApplication.class, args);
+	}
+
+}
+
+//teste
