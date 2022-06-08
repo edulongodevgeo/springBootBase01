af0f35abb98628fd55e6bb367e4afc798d5c66bb
edulongodevgeo <eduardosl.geo@gmail.com>
2022-06-08 20:30:35 -0300

    Add classes e enums

---
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/model/Client.java
@@ -0,0 +1,123 @@
+package com.teste.primeiroexemplo.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import enums.ClientType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    /**
+     *
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String cpf;
+    private String password;
+    private String email;
+    private String cellphone;
+    private DateTimeFormat birthDate;
+    private Double monthlyIncome;
+    private ClientType clientType;
+    private Address address;
+
+    public Client() {
+    }
+
+    public Client(String name, String cpf, String password, String email, String cellphone, DateTimeFormat birthDate, ClientType clientType, Address address) {
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.cellphone = cellphone;
+        this.email = email;
+        this.password = password;
+        this.clientType = clientType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public DateTimeFormat getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(DateTimeFormat birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(Double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public ClientType getClientType() {
+        return clientType;
+    }
+
+    public void setClientType(ClientType clientType) {
+        this.clientType = clientType;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
---
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/model/DebitCard.java
@@ -0,0 +1,5 @@
+package com.teste.primeiroexemplo.model;
+
+public class DebitCard {
+
+}
